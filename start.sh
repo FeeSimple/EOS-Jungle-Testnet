f5cd4954dbe3e4f802d043627fafaf8a72b5c92e
trung8x <trungtt1981@gmail.com>
2018-04-27 23:12:56 +0700

    Adapt scripts

--- start.sh
+++ start.sh
@@ -7,13 +7,14 @@
 ###############################################################################
 
 
-NODEOSBINDIR="/home/eos-dawn-v3.0.0/eos/build/programs/nodeos"
-DATADIR="/opt/JungleTestnet"
+#NODEOSBINDIR="/home/eos-dawn-v3.0.0/eos/build/programs/nodeos"
+DATADIR="~/nodeos_data/trung"
+CONFIGDIR="./"
 
-$DATADIR/stop.sh
-echo -e "Starting Nodeos \n";
+#./stop.sh
+#echo -e "Starting Nodeos \n";
 
-$NODEOSBINDIR/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+nodeos --data-dir $DATADIR --config-dir $CONFIGDIR # > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
 #$NODEOSBINDIR/nodeos --replay $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
 #$NODEOSBINDIR/nodeos --enable-stale-production true $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
 #$EOSIOBINDIR/nodeos --enable-stale-production true --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
